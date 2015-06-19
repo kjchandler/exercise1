566a3bcca9290fc8c99305886de56550d543e3dd
Kelly Chandler <chandlerkelly@hotmail.com>
2015-06-19 11:49:17 -0400

    complete

---
+++ exercise.js
@@ -0,0 +1,146 @@
+
+//1.string value named Rob.
+var name = "Rob"; 
+
+
+//2.concatenate the word help and name
+var greeting = "hello" + " "+name;
+
+//3evaluate 
+
+//4comment each question
+
+//5.create a function that logs brainstation whenever its called
+
+function name(name){
+	var x = name;
+	return x;
+
+};
+
+//6. call the function from 5
+
+name("brainstation");
+console.log(x);
+
+//7. create a function with the paramater that logs hello space name
+function createGreeting(greeting, name) {
+  var formattedGreeting = greeting + " " + name + "!";
+  return formattedGreeting;
+};
+
+var greeting = createGreeting("Hello", "Kelly");
+
+console.log(greeting);
+
+
+//8. Create a function that has 2 paramaters a and b. return the boolean a and b being true and flash
+
+
+var a = (function(a, b) {
+  
+  var x = a + " " + b + "!";
+  console.log(x);
+});
+
+a("true", "false");
+
+//9. We use git to communicate changes that occur to other developers.
+
+//10. Write a function that takes a number as a value. greater than equal to 2.
+  
+function x(value){
+	if(num > 2);
+
+
+};
+console.log(x);
+
+//11. write the code that will log Hello World 12 times.
+(var i =0; i < 12; i++){
+
+	console.log("Hello World");
+};
+
+//12. create an array of number array. save it in the var my numbers
+
+var myNumbers= [0,1,2,3,4,5];
+
+//13. iterate over the numbers and sum them
+var sum =0;
+for(var i=0; i < myNumbers.length; i++){
+	var sum = sum + myNumbers[i];
+};
+
+//14. Add the values 12 to 55 to the array myNumbers
+for(var i = 12; 12 <= 55; i++){
+	var x = myNumbers.push[i];
+};
+
+//15 add the values 12 to 55 to myNumbers
+
+
+
+//create a constructor function that has the name Person with parameters
+//name and address
+
+function Person(name, address){
+	this:name = name;
+	this:address=address;
+}
+
+//15.create an object 
+var person1 = new Person("Kelly", "103 Union");
+
+//16. create an object using a con func called computer memory and screen size method turn on 
+
+function Computer(memory, screenSize){
+	this:memory = memory;
+	this:screenSize = screenSize;
+};
+
+//17create a computer, 16 gigs, 24 inches call its method turn on.
+		
+	function Computer(memory, screenSize){
+		this:memory = memory;
+		this:screenSize = screenSize;
+};
+		var values = Computer("16 gigs", "24 inches");
+		console.log(values);
+
+//18. create an object called computer Store name and address
+//method make computer that will make a computer with memory and screen siz.
+
+
+function ComputerStore(name, address){
+	this:memory = memory;
+	this:address = address;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
